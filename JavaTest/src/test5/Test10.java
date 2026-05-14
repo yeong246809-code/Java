4972121bbae30531130375af5b96826fbb2bee43
yeong246809 <yeong246809@gmail.com>
2026-05-14 12:24:03 +0900

    update test

---
+++ JavaTest/src/test5/Test10.java
@@ -0,0 +1,31 @@
+package test5;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Test10 {
+    public static void main(String[] args) {
+        String path = "C:\\Users\\GGG\\Desktop\\Result.txt";
+        
+        try {
+            File file = new File(path);
+            FileWriter fw = new FileWriter(file);
+            
+            for(int x=2 ; x<=9 ; x++) {
+                fw.write(x + "단" + "\n");;
+                for(int y=1 ; y<=9 ; y++) {
+                    int z = x * y;
+                    fw.write(x + " x " + y +" = " + z + "\n");
+                }
+            }
+            
+            fw.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("구구단 파일 출력 완료...");
+    }
+}
